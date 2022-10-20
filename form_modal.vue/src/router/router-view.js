d107ae8a23caf4b7177cfdc5a9393a1978dc3052
tangytin <tangytin@163.com>
2022-10-20 21:23:24 +0800

    fix : 修复拼写错误，history模式404原因以及解决方案

--- form_modal.vue/src/router/router-view.js
+++ form_modal.vue/src/router/router-view.js
@@ -1,8 +1,8 @@
 export default {
     render(h) {
-        this.$vnode.data.routerView = true
-        let dept = 0
-        let parent = this.$parent
+        this.$vnode.data.routerView = true                                  //通过routerView来记录是否已经被访问
+        let dept = 0                                                        //当前路由深度
+        let parent = this.$parent                                           //获取当前路由组件的父路由组件，直到找到parent为undefined则深度寻找完毕，并且通过dept来记录深度
         while(parent){
             const vNodeData = parent.$vnode ? parent.$vnode.data:{}
             if(vNodeData){
@@ -12,14 +12,25 @@ export default {
             }
             parent = parent.$parent
         }
-        console.log("dept",dept)
 
         // const { routeMap, current } = this.$router
-        let Component =  null
-        let route = this.$router.matched[dept]
+        let Component =  null                                               //初始化要渲染出来的component为null
+        let route = this.$router.matched[dept]                              //获取对应深度的路由
         if(route){
-            Component = route.component
-        }
-        return h(Component)
+            Component = route.component                                     //根据路由定位到组件
+        }   
+        return h(Component)                                                 //render渲染虚拟DOM
     }
+}
+
+// history模式下刷新为什么会出现404的情况：因为重新刷新，http server会直接从当前url下寻找资源，但是spa(单页面应用)的路由是虚拟的，所以找不到资源(404)。那么就需要重定位让他从根路径下寻找
+// history模式下避免404解决方式：
+module.exports = {
+    publicPath: '/', //这个必须，引入静态资源需要从根路径引入，否则会找不到静态资源
+    devServer: {
+        // history模式下的url会请求到服务器端，但是服务器端并没有这一个资源文件，就会返回404，所以需要配置这一项
+        historyApiFallback: {
+            index: '/index.html' //与output的publicPath
+        },
+    },
 }
--- form_modal.vue/src/router/vue-router.js
+++ form_modal.vue/src/router/vue-router.js
@@ -2,15 +2,15 @@ import Link from './router-link'
 import View from './router-view'
 // 挂在Vue-router
 let Vue;
-class ViewRouter {
+class VueRouter {
     constructor(option) {
         this.$options = option
 
         // 实现监听hash模式下url的变化，然后改变current
-        const initialHashPath = window.location.hash.slice(1)
-        Vue.util.defineReactive(this, 'current', initialHashPath)
-        Vue.util.defineReactive(this,'matched',[])
-        this.match()
+        const initialHashPath = window.location.hash.slice(1)       //hash值: #及后面的部分。
+        Vue.util.defineReactive(this, 'current', initialHashPath)   //响应式定义一个current,当current改变的时候路由会根据对应的path找到对应的component来局部更新页面
+        Vue.util.defineReactive(this,'matched',[])                  //matched数组，用来存放当前路由以及前面所有的父级路由
+        this.match()                                                //开始递归匹配路由
         window.addEventListener('hashchange', this.onHashChange.bind(this))
         window.addEventListener('load', this.onHashChange.bind(this))
 
@@ -26,7 +26,7 @@ class ViewRouter {
         this.match()
     }
     match(routes){
-        routes = routes || this.$options.routes
+        routes = routes || this.$options.routes                                 //当routes不存在，默认为我们配置的所有routes(this.$options.routes)
         for (const route of routes) {
             if(route.path === '/' && this.current === '/'){
                 this.matched.push(route)
@@ -41,7 +41,7 @@ class ViewRouter {
         }
     }
 }
-ViewRouter.install = function(_Vue) {
+VueRouter.install = function(_Vue) {
     // 保存构造函数，在VueRouter中使用 
     Vue = _Vue
 
@@ -49,15 +49,15 @@ ViewRouter.install = function(_Vue) {
     // mixin混入可以获取所有的组件
     Vue.mixin({
         beforeCreate() {
-            if (this.$options.router) {
-                Vue.prototype.$router = this.$options.router
+            if (this.$options.router) {                             
+                Vue.prototype.$router = this.$options.router            //将router挂载到Vue上(这样可以通过this.$router来访问router.router中包含了路由的)
             }
         },
     })
 
-    Vue.component('router-link', Link)
+    Vue.component('router-link', Link)                                  //自定义router-link组件
 
-    Vue.component('router-view', View)
+    Vue.component('router-view', View)                                  //自定义router-view组件
 }
 
-export default ViewRouter
+export default VueRouter
